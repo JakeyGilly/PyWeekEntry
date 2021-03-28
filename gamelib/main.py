63647186ebdf36954260e8f1d2f8add208ba7857
Finn O'Neill <56732347+Explorer017@users.noreply.github.com>
2021-03-28 17:56:49 +0100

    fixed the square
    the square is now fixed

--- gamelib/main.py
+++ gamelib/main.py
@@ -9,26 +9,36 @@ def main():
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption("raylib on c++ is better")
     red = 255, 0, 0
-    car = pygame.draw.rect(screen, red, ((screenwidth / 2) - 25, carHeight, 50, 50))
-    car0 = car.copy()
-    drawstartroad(screen)
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
-            pygame.display.update()
-        car0.y -= 10
-        pygame.draw.rect(screen, red, car0, 1)
-        pygame.display.update()
 
-class Road():
-    def __init__(self,length):
-        self.length = length
+        # Update Game Logic
+        carHeight -= 0.25
         
 
-def drawroad(screen, length, width, direction):
-    pass
+        # Clear the screen
+        screen.fill((255, 255, 255))
+
+        # Draw the things on the screen
+        drawstartroad(screen)
+        pygame.draw.rect(screen, red, ((screenwidth / 2) - 25, carHeight, 50, 50))
+        pygame.display.flip()
+        
+
+        # Update the screen
+        pygame.display.update()
+
+
+#class Road():
+#    def __init__(self,length):
+#        self.length = length
+        
+
+#def drawroad(screen, length, width, direction):
+#    pass
 def drawstartroad(screen):
     length = 200
     width = 10
-    pygame.draw.line(screen, (0, 255, 0), ((screenwidth/4)*3, screenheight) , ((screenwidth/4)*3, 0), width)
-    pygame.draw.line(screen, (0, 255, 0), (screenwidth/4, screenheight) , (screenwidth/4, 0), width)
+    pygame.draw.line(screen, (86,86,86), ((screenwidth/4)*3, screenheight) , ((screenwidth/4)*3, 0), width)
+    pygame.draw.line(screen, (86,86,86), (screenwidth/4, screenheight) , (screenwidth/4, 0), width)
