90d9edd140a8b637d7dd87ed1fac08dfaf1c36fb
Jake Gillman <57807696+JakeyGilly@users.noreply.github.com>
2021-03-28 20:29:06 +0100

    added rotating the car

--- gamelib/main.py
+++ gamelib/main.py
@@ -8,6 +8,7 @@ screenwidth = 854
 
 def main():
     # Set player starting X, Y
+    angle = 0
     player.x = (screenwidth / 2) - 25
     player.y = (screenheight / 2) - 25
     # Setting up the screen
@@ -15,18 +16,19 @@ def main():
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption("Chase")
     #Detecting for Quit and Key Press events
-    player.playerX_change = 0
+    player.playerangle = 0
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
             if event.type == pygame.KEYDOWN: player.updatePlayerDown(event.key)
             if event.type == pygame.KEYUP: player.updatePlayerUp(event.key)
-        player.x += player.playerX_change
+        angle += player.playerangle
         # Clear the screen
         screen.fill((255, 255, 255))
         # Draw the things on the screen
         roads.drawstartroad(screen)
-        pygame.draw.rect(screen, (255, 0, 0), (player.x, player.y, 50, 50))
+        #pygame.draw.rect(screen, (0, 0, 255), (player.x, player.y, 50, 50))
+        player.rectRotate(screen, (255, 0, 0), (player.x, player.y, 50, 70), angle)
         pygame.display.flip()
         # Update the screen
         pygame.display.update()
--- gamelib/player.py
+++ gamelib/player.py
@@ -8,10 +8,21 @@ y = 0
 
 def updatePlayerDown(key):
     if key == pygame.K_a:
-        player.playerX_change = -0.1
+        player.playerangle = -0.1
     elif key == pygame.K_d:
-        player.playerX_change = 0.1
+        player.playerangle = 0.1
 
 def updatePlayerUp(key):
     if key == pygame.K_a or key == pygame.K_d:
-        player.playerX_change = 0
+        player.playerangle = 0
+
+def rectRotate(surface, color, pos, rotation_angle):
+    s = pygame.Surface((pos[2]*2,pos[3]*2))
+    s = s.convert_alpha()
+    s.fill((0,0,0,0))
+    pygame.draw.rect(s, color, ((pos[2]-pos[2]//2),(pos[3]-pos[3]//2),pos[2],pos[3]))
+    s = pygame.transform.rotate(s, -rotation_angle)
+    incfromrotw = (s.get_width()-pos[2]*2)//2
+    incfromroth = (s.get_height()-pos[3]*2)//2
+    surface.blit(s, (pos[0]-pos[2]+pos[2]//2-incfromrotw,pos[1]-pos[3]+pos[3]//2-incfromroth))
+        
