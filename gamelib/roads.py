90768414c7358e6841ffc343c3ffc9d8151bd384
Finn O'Neill <56732347+Explorer017@users.noreply.github.com>
2021-03-30 19:10:50 +0100

    made the road a rectangle instead of 2 lines

--- gamelib/roads.py
+++ gamelib/roads.py
@@ -1,11 +1,8 @@
 from gamelib import main
 import pygame
 
-def drawstartroad(screen):
-    length = 200
-    width = 10
-    pygame.draw.line(screen, (86,86,86), ((main.screenwidth/4)*3, main.screenheight) , ((main.screenwidth/4)*3, 0), width)
-    pygame.draw.line(screen, (86,86,86), (main.screenwidth/4, main.screenheight) , (main.screenwidth/4, 0), width)
+def drawstartroad(screen): 
+    pygame.draw.rect(screen, (86,86,86),((main.screenwidth//2)//2,0,main.screenwidth//2,main.screenheight))
 
 #class Road():
 #    def __init__(self,length):
